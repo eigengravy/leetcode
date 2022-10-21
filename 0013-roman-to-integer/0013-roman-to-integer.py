ae55432ca1f7688b94074188d01a44dab690ceaf
Sarang S <85063520+eigengravy@users.noreply.github.com>
2022-10-21 11:17:37 +0530

    Time: 52 ms (90.53%), Space: 13.8 MB (76.95%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,11 +2,11 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         mapping = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         digits = list(map(lambda x: mapping[x], s))
-        digits.append(0)
         number = 0
-        for curr_digit, next_digit in zip(digits[:-1],digits[1:]):
-            if curr_digit >= next_digit:
-                number += curr_digit
+        for i in range(len(digits)-1):
+            if (digits[i] >= digits[i+1]):
+                number += digits[i]
             else:
-                 number -= curr_digit
+                 number -= digits[i]
+        number += digits[-1]
         return number
