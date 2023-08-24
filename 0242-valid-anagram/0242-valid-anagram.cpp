c58030920b201772fdde101e1c1f60273c99de72
Sarang S <85063520+eigengravy@users.noreply.github.com>
2023-08-24 16:18:03 +0530

    Time: 15 ms (32.54%), Space: 7.3 MB (48.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,16 +2,9 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-        unordered_map<char, int> cmap;
-        // unordered_map<char, int> s_map;
-        // unordered_map<char, int> t_map;
-        for(int i=0; i<s.size(); i++){
-            cmap[s[i]]++;
-            cmap[t[i]]--;
-        }
-        for(int i=0; i< cmap.size(); i++){
-            if(cmap[i] != 0) return false;
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if (s == t) return true;
+        return false;
     }
 };
