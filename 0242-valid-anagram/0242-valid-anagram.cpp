73d1572ea99bab381be234b291b8e40da2a694e7
Sarang S <85063520+eigengravy@users.noreply.github.com>
2023-08-24 16:16:10 +0530

    Time: 9 ms (67.69%), Space: 7.9 MB (5.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,14 +4,10 @@ public:
         if(s.size()!=t.size()) return false;
         unordered_map<char, int> s_map;
         unordered_map<char, int> t_map;
-        
-        for(char it: s){
-            s_map[it]++;
+        for(int i=0; i<s.size(); i++){
+            s_map[s[i]]++;
+            t_map[t[i]]++;
         }
-        for(char it: t){
-            t_map[it]++;
-        }
-
         for(int i=0; i<s_map.size(); i++){
             if(s_map[i]!=t_map[i]) return false;
         }
