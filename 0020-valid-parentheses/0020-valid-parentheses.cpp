7b65b81cfe2a921504e70ec4d529b6d032d032bd
Sarang S <85063520+eigengravy@users.noreply.github.com>
2023-08-25 19:45:27 +0530

    Time: 0 ms (100%), Space: 6.4 MB (38.21%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,56 +1,20 @@
 class Solution {
 public:
-    bool isValid(string str) {
-        stack<int> s;
-        for (char it: str){
-            switch(it){
-                case '(':
-                    s.push(0);
-                    break;
-                case '[':
-                    s.push(1);
-                    break;
-                case '{':
-                    s.push(2);
-                    break;
-                case ')':
-                    if (s.empty()) {
-                      return false;
-                    }
-                    if (s.top() == 0) {
-                        s.pop();
-                    } else {
-                        return false;
-                    }
-                    break;
-                case ']':
-                    if (s.empty()) {
-                      return false;
-                    }
-                    if (s.top() == 1) {
-                        s.pop();
-                    } else {
-                        return false;
-                    }
-                    break;
-                case '}':
-                    if (s.empty()) {
-                      return false;
-                    }
-                    if (s.top() == 2) {
-                        s.pop();
-                    } else {
-                        return false;
-                    }
-                    break;
-                default:
-                break;
+    bool isValid(string s) {
+        stack<char> st; 
+        for (char it : s) { 
+            if (it == '(' || it == '{' || it == '[') { 
+                st.push(it);
+            } else { 
+                if (st.empty() || 
+                    (it == ')' && st.top() != '(') || 
+                    (it == '}' && st.top() != '{') ||
+                    (it == ']' && st.top() != '[')) {
+                    return false; 
+                }
+                st.pop(); 
             }
         }
-        if (s.empty()) {
-            return true;
-        } else {
-            return false;
-        }   
+        return st.empty(); 
     }
 };
